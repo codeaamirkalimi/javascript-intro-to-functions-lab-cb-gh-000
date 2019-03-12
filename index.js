27b48695c63d972df44784980c2c451253cb98ac
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-12 18:01:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-function shout(string){
-  return string.toUpperCase();
+function shout(string) {
+    return string.toUpperCase()
 }
 
-function whisper(string){
-  return string.toLowerCase();
+function whisper(string) {
+    return string.toLowerCase()
 }
 
-function logShout(string){
-  console.log(string);
-}
-function sayHiToGrandma(string){
-  var stringLowerCase = string.toLowerCase();
-  if(stringLowerCase === string){
-    return "I can't hear you!";
-  }
-}
-function sayHiToGrandma(string){
-  var stringUpperCase = string.toUpperCase();
-  if(stringUpperCase === string){
-    return "YES INDEED!";
-  }
+function logShout(string) {
+    console.log(string.toUpperCase())
 }
 
-function sayHiToGrandma(string){
-  var stringLoveYour = "I love you, Grandma.";
-  if(stringLoveYour === string){
-    return "I love you, too.";
-  }
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+    if (string === "I love you, Grandma.") {
+        return "I love you, too."
+    }
+
+    if (string.toLowerCase() === string) {
+        return "I can't hear you!"
+    }
+
+    if (string.toUpperCase() === string) {
+        return "YES INDEED!"
+    }
 }
